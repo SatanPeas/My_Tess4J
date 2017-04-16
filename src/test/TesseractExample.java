42885dfea53dd04d2b3bc78b478163469a631b7d
SatanPeas <doudou_julius@outlook.com>
2017-04-16 16:48:54 +0800

    some modify

--- src/test/TesseractExample.java
+++ src/test/TesseractExample.java
@@ -2,11 +2,8 @@ package test;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
 
@@ -26,32 +23,19 @@ public class TesseractExample {
         
         BufferedImage readFile = null;
 		try {
-			readFile = ImageIO.read(new FileInputStream(imageFile));
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-        BufferedImage imgFile = ImageHelper.convertImageToGrayscale(readFile);
-        int endX = imgFile.getWidth();
-        int endY = imgFile.getHeight();
-        imgFile = ImageHelper.getScaledInstance(imgFile, endX * 5, endY * 5);
-        try {
-            String result = instance.doOCR(imgFile);
+			readFile = ImageIO.read(imageFile);
+			BufferedImage imgFile = ImageHelper.convertImageToGrayscale(readFile);
+	        int endX = imgFile.getWidth();
+	        int endY = imgFile.getHeight();
+	        imgFile = ImageHelper.getScaledInstance(imgFile, endX * 5, endY * 5);
+	        String result = instance.doOCR(imgFile);
             System.out.println(result);
-        } catch (TesseractException e) {
-            System.err.println(e.getMessage());
-        }
-        
-       File file = new File("F:\\test.png");
-       try {
-		ImageIO.write(imgFile, "png", file);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TesseractException e) {
+			e.printStackTrace();
+		}
     }
 }
